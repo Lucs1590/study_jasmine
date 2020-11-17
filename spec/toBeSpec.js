82a16d8431116f836201c97e91b4608e915bd9c5
Lucas Brito <lucasbsilva29@gmail.com>
2020-11-16 22:00:17 -0300

    refactor(toBeSpec.js): uses to use

--- spec/toBeSpec.js
+++ spec/toBeSpec.js
@@ -1,7 +1,7 @@
 describe('To Be Comparator ', () => {
-    it('should validate the To Be uses', () => {
-        const obj1 = { value: true};
-        const obj2 = { value: true};
+    it('should validate the To Be use', () => {
+        const obj1 = { value: true };
+        const obj2 = { value: true };
 
         expect(true).toBe(true);
         expect('Lucas').toBe('Lucas');
