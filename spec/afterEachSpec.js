f78dc1cbe942dc59eaa18f76b89e15d9d4b9d9d2
Lucas Brito <lucasbsilva29@gmail.com>
2020-12-06 23:27:52 -0300

    refactor: using function callback

--- spec/afterEachSpec.js
+++ spec/afterEachSpec.js
@@ -1,20 +1,20 @@
-describe('After Each Test', () => {
+describe('After Each Test', function () {
 
     var count = 0;
 
-    beforeEach(() => {
+    beforeEach(function () {
         count += 1;
     });
 
-    afterEach(() => {
+    afterEach(function () {
         count = 0;
     });
 
-    it('should ensure that count is equal 1', () => {
+    it('should ensure that count is equal 1', function () {
         expect(count).toEqual(1);
     });
 
-    it('should ensure that count is yet equal 1', () => {
+    it('should ensure that count is yet equal 1', function () {
         expect(count).toEqual(1);
     });
 });
--- spec/beforeEachSpec.js
+++ spec/beforeEachSpec.js
@@ -1,16 +1,16 @@
-describe('Before Each Test', () => {
+describe('Before Each Test', function () {
 
     var count = 0;
 
-    beforeEach(() => {
+    beforeEach(function () {
         count += 1;
     });
 
-    it('should increment count to 1', () => {
+    it('should increment count to 1', function () {
         expect(count).toEqual(1);
     });
 
-    it('should increment count to 2', () => {
+    it('should increment count to 2', function () {
         expect(count).toEqual(2);
     });
 });
