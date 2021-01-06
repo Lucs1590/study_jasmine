2a63268f7e6ae7ee2312af9ca384d2477ee18df5
Lucas Brito <lucasbsilva29@gmail.com>
2021-01-05 22:03:58 -0300

    feat: add align tests
    using a suite inside other

---
+++ spec/alignSuitsSpec.js
@@ -0,0 +1,24 @@
+describe('External Suite Test', () => {
+    let externalCount = 0;
+
+    beforeEach(() => {
+        externalCount++;
+    });
+
+    it('should contain 1 to externalCount', () => {
+        expect(externalCount).toEqual(1);
+    });
+
+    describe('Internal Suite Test', () => {
+        let internalCount = 0;
+
+        beforeEach(() => {
+            internalCount++;
+        });
+
+        it('should validate counts values', () => {
+            expect(internalCount).toEqual(1);
+            expect(externalCount).toEqual(2);
+        });
+    });
+});
