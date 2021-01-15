35b90a1f7ed5b1332f017db762b9c053a82e8fad
Lucas Brito <lucasbsilva29@gmail.com>
2021-01-14 23:37:40 -0300

    feat(spyCalledTimesSpec.js): learn about have been called times

---
+++ spec/spyCalledTimesSpec.js
@@ -0,0 +1,17 @@
+describe('toHaveBeenCalledTimes test', () => {
+    let calculator = {
+        sum: function(n1,n2) {
+            return n1 + n2;
+        }
+    }
+
+    beforeAll(() => {
+        spyOn(calculator, 'sum');
+    });
+
+    it('should call "sum" method 2 times', () => {
+        calculator.sum(1,1);
+        calculator.sum(3,1);
+        expect(calculator.sum).toHaveBeenCalledTimes(2);
+    });
+});
