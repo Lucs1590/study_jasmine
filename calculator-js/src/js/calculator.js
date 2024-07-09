69b1642606549c32f4b2ab0bf8cdf7b40f595d7c
Lucas Brito <lucasbsilva29@gmail.com>
2024-07-09 13:23:33 -0300

    feat: improving code to use bootstrap 5

---
+++ calculator-js/src/js/calculator.js
@@ -0,0 +1,18 @@
+var Calculator = {
+    sum: function (a, b) {
+        return a + b;
+    },
+    subtract: function (a, b) {
+        return a - b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    multiply: function (a, b) {
+        return a * b;
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Calculator;
+}
